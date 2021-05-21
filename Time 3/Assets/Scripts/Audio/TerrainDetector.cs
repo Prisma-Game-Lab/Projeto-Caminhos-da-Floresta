6aef35b834cb2b220b708ed885bab2f509ebf0a9
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2021-05-21 12:57:58 -0300

    Detectando qualquer terrain (pegando distancia ao centro)

--- Time 3/Assets/Scripts/Audio/TerrainDetector.cs
+++ Time 3/Assets/Scripts/Audio/TerrainDetector.cs
@@ -2,48 +2,63 @@ using UnityEngine;
 
 public class TerrainDetector
 {
-    private TerrainData terrainData;
-    private int alphamapWidth;
-    private int alphamapHeight;
-    private float[,,] splatmapData;
-    private int numTextures;
-
-    public TerrainDetector()
+    private Terrain GetClosestCurrentTerrain(Vector3 playerPos)
     {
-        terrainData = Terrain.activeTerrain.terrainData;
-        alphamapWidth = terrainData.alphamapWidth;
-        alphamapHeight = terrainData.alphamapHeight;
+        //Get the closest one to the player
+        Terrain[] _terrains = Terrain.activeTerrains;
+        var center = new Vector3(_terrains[0].transform.position.x + _terrains[0].terrainData.size.x / 2, playerPos.y, _terrains[0].transform.position.z + _terrains[0].terrainData.size.z / 2);
+    
+        float lowDist = (center - playerPos).sqrMagnitude;
+        var terrainIndex = 0;
 
-        splatmapData = terrainData.GetAlphamaps(0, 0, alphamapWidth, alphamapHeight);
-        numTextures = splatmapData.Length / (alphamapWidth * alphamapHeight);
+        for (int i = 0; i < _terrains.Length; i++)
+        {
+            center = new Vector3(_terrains[i].transform.position.x + _terrains[i].terrainData.size.x / 2, playerPos.y, _terrains[i].transform.position.z + _terrains[i].terrainData.size.z / 2);
+
+            //Find the distance and check if it is lower than the last one then store it
+            var dist = (center - playerPos).sqrMagnitude;
+            if (dist < lowDist)
+            {
+                lowDist = dist;
+                terrainIndex = i;
+            }
+        }
+        return _terrains[terrainIndex];
     }
 
-    private Vector3 ConvertToSplatMapCoordinate(Vector3 worldPosition)
+    private Vector3 ConvertToSplatMapCoordinate(Vector3 worldPosition, Terrain terrain)
     {
         Vector3 splatPosition = new Vector3();
-        Terrain ter = Terrain.activeTerrain;
-        Vector3 terPosition = ter.transform.position;
-        splatPosition.x = ((worldPosition.x - terPosition.x) / ter.terrainData.size.x) * ter.terrainData.alphamapWidth;
-        splatPosition.z = ((worldPosition.z - terPosition.z) / ter.terrainData.size.z) * ter.terrainData.alphamapHeight;
+        Vector3 terPosition = terrain.transform.position;//GetPosition()?
+        splatPosition.x = ((worldPosition.x - terPosition.x) / terrain.terrainData.size.x) * terrain.terrainData.alphamapWidth;
+        splatPosition.z = ((worldPosition.z - terPosition.z) / terrain.terrainData.size.z) * terrain.terrainData.alphamapHeight;
         return splatPosition;
     }
 
     public int GetActiveTerrainTextureIdx(Vector3 position)
     {
-        Vector3 terrainCord = ConvertToSplatMapCoordinate(position);
-        int activeTerrainIndex = 0;
+        Terrain currentTerrain = GetClosestCurrentTerrain(position);
+
+        TerrainData terrainData = currentTerrain.terrainData;
+        int alphamapWidth = terrainData.alphamapWidth;
+        int alphamapHeight = terrainData.alphamapHeight;
+
+        float[,,] splatmapData = terrainData.GetAlphamaps(0, 0, alphamapWidth, alphamapHeight);
+        int numTextures = splatmapData.Length / (alphamapWidth * alphamapHeight);
+        
+        Vector3 terrainCord = ConvertToSplatMapCoordinate(position, currentTerrain);
+        int activeTerrainTextureIndex = 0;
         float largestOpacity = 0f;
 
         for (int i = 0; i < numTextures; i++)
         {
             if (largestOpacity < splatmapData[(int)terrainCord.z, (int)terrainCord.x, i])
             {
-                activeTerrainIndex = i;
+                activeTerrainTextureIndex = i;
                 largestOpacity = splatmapData[(int)terrainCord.z, (int)terrainCord.x, i];
             }
         }
 
-        return activeTerrainIndex;
+        return activeTerrainTextureIndex;
     }
-
 }
