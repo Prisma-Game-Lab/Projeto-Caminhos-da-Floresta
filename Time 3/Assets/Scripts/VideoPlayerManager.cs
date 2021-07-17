40911ddec17839b356f332d2b38be2a1ca6e339b
NagibSuaid <nagibmoura@gmail.com>
2021-07-16 21:08:46 -0300

    Switching clips and animation for credits

--- Time 3/Assets/Scripts/VideoPlayerManager.cs
+++ Time 3/Assets/Scripts/VideoPlayerManager.cs
@@ -8,15 +8,29 @@ using UnityEngine.Video;
 public class VideoPlayerManager : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public VideoClip story;
+    public VideoClip openEmpty;
+    public VideoClip closeEmpty;
     private void Awake()
     {
         videoPlayer.Play();
         videoPlayer.Pause();
         videoPlayer.frame = 1;
     }
-
-    public void QueueSceneChange(string sceneName)
+    public void PlayOpen()
     {
+        videoPlayer.clip = openEmpty;
+        videoPlayer.Play();
+    }
+    public void PlayClose()
+    {
+        videoPlayer.clip = closeEmpty;
+        videoPlayer.Play();
+    }
+
+    public void PlayStartThenChangeScene(string sceneName)
+    {
+        videoPlayer.clip = story;
         videoPlayer.Play();
         StartCoroutine(ChangeSceneOnVideoEnd(sceneName));
     }
