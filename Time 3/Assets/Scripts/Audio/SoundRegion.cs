983e21c2c2a0385785a2ccc70c20ecadd28c4924
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2021-07-25 13:29:05 -0300

    add ambiencia pedras scene

--- Time 3/Assets/Scripts/Audio/SoundRegion.cs
+++ Time 3/Assets/Scripts/Audio/SoundRegion.cs
@@ -14,7 +14,6 @@ public class SoundRegion : MonoBehaviour
     {
         ambience = FMODUnity.RuntimeManager.CreateInstance(ambienceIntToName());
         player = GameObject.FindGameObjectWithTag("Player");
-        //ambience.start();
     }
 
     // Update is called once per frame
@@ -27,7 +26,6 @@ public class SoundRegion : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //Debug.Log("entrou");
             PlayAmbience();
         }
     }
@@ -39,7 +37,6 @@ public class SoundRegion : MonoBehaviour
 
         if (state != FMOD.Studio.PLAYBACK_STATE.PLAYING)
         {
-            //Debug.Log("tocou");
             FMODUnity.RuntimeManager.AttachInstanceToGameObject(ambience, GetComponent<Transform>(), GetComponent<Rigidbody>());
 
             ambience.setParameterByName("PlayerDistance", 1);
@@ -75,16 +72,25 @@ public class SoundRegion : MonoBehaviour
     {
         switch (ambienceType){
             case 0: //aberto
-                return "event:/ambiencia/aberto";
+                return "event:/ambiencia/0 - aberto";
             case 1: //aberto + rio
-                return "event:/ambiencia/aberto + rio";
+                return "event:/ambiencia/1 - aberto + rio";
             case 2: //fechado
-                return "event:/ambiencia/fechado";
+                return "event:/ambiencia/2 - fechado";
             case 3: //vento arvore
-                return "event:/ambiencia/vento arvore";
+                return "event:/ambiencia/3 - vento arvore";
             case 4: //ventro montanha
-                return "event:/ambiencia/vento montanha";
+                return "event:/ambiencia/4 - vento montanha";
+            case 5: //noite
+                return "event:/ambiencia/5 - noite";
+            case 6: //sinistro
+                return "event:/ambiencia/6 - creepy";
+            case 7: //coruja
+                return "event:/ambiencia/7 - coruja";
+            case 8: //sapo
+                return "event:/ambiencia/8 - sapo";
             default:
+                Debug.Log("evento de ambiencia nao encontrado!");
                 return "event:/ambiencia/aberto";
         }
     }
